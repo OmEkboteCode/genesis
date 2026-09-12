f67a2c97e16835fe7de54ca9a194c4352d8633d4
OmEkboteCode <omekbote007@gmail.com>
2026-09-12 13:32:49 +0530

    Add Repository Index

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const Repository = require("./models/repository.js");
+const ejsMate = require("ejs-mate")
 // const { repositorySchema } = require("./schema.js")
 
 const MONGO_URL = `mongodb://127.0.0.1:27017/genesis`;
@@ -24,24 +25,17 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate)
 app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
   res.send("Working");
 });
 
-// app.get("/testrepo", async (req, res) => {
-//     let sampleRepo = new Repository({
-//         name: "Genesis",
-//         description: "A developer collaboration platform built from the ground up while exploring modern web development and software engineering.",
-//         language: "Javascript",
-//         visibility: "Public",
-//         owner: "Om",
-//     });
-//     await sampleRepo.save();
-//     console.log("Sample Was Saved");
-//     res.send("Successful Testing")
-// })
+app.get("/repositories", async (req, res) => {
+    const allRepository = await Repository.find({});
+    res.render("repositories/index.ejs", { allRepository})
+})
 
 app.listen(3000, () => {
   console.log("Server is Listening to Port 3000");
