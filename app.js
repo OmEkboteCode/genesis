063ad7ff25808aa89395d92098c979f92c276fbc
OmEkboteCode <omekbote007@gmail.com>
2026-09-12 14:39:00 +0530

    Add Repository Details

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const Repository = require("./models/repository.js");
-const ejsMate = require("ejs-mate")
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const ejsMate = require("ejs-mate");
 // const { repositorySchema } = require("./schema.js")
 
 const MONGO_URL = `mongodb://127.0.0.1:27017/genesis`;
@@ -25,19 +27,30 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine("ejs", ejsMate)
+app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
   res.send("Working");
 });
 
-app.get("/repositories", async (req, res) => {
+app.get(
+  "/repositories",
+  wrapAsync(async (req, res) => {
     const allRepository = await Repository.find({});
-    res.render("repositories/index.ejs", { allRepository})
-})
+    res.render("repositories/index.ejs", { allRepository });
+  }),
+);
+
+app.get(
+  "/repositories/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const repository = await Repository.findById(id);
+    res.render("repositories/show.ejs", { repository });
+  }),
+);
 
 app.listen(3000, () => {
   console.log("Server is Listening to Port 3000");
 });
-
