dd6a7efc4d5c0a4e5ac25372a51997410a67fb3a
OmEkboteCode <omekbote007@gmail.com>
2026-09-26 17:50:47 +0530

    Add User Registration

--- routes/user.js
+++ routes/user.js
@@ -3,18 +3,19 @@ const router = express.Router({ mergeParams: true });
 const User = require("../models/user.js");
 const Repository = require("../models/repository.js");
 const wrapAsync = require("../utils/wrapAsync.js");
-const ExpressError = require("../utils/ExpressError.js");
-const { userSchema } = require("../schema.js");
+// const ExpressError = require("../utils/ExpressError.js");
+// const { userSchema } = require("../schema.js");
+const passport = require("passport");
 
-const validateUser = (req, res, next) => {
-  let { error } = userSchema.validate(req.body);
-  if (error) {
-    let errorMessage = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, errorMessage);
-  } else {
-    next();
-  }
-};
+// const validateUser = (req, res, next) => {
+//   let { error } = userSchema.validate(req.body);
+//   if (error) {
+//     let errorMessage = error.details.map((el) => el.message).join(",");
+//     throw new ExpressError(400, errorMessage);
+//   } else {
+//     next();
+//   }
+// };
 
 // Users
 
@@ -44,6 +45,24 @@ router.delete(
     let { id } = req.params;
     await User.findByIdAndDelete(id);
     res.redirect("/users");
+  }),
+);
+
+router.get("/signup", (req, res) => {
+  res.render("users/signup.ejs");
+});
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+      let { username, email, password } = req.body;
+      const newUser = new User({ email, username });
+      const registeredUser = await User.register(newUser, password)
+      console.log(registeredUser);
+      req.flash("success", "Welcome To Genesis!");
+      res.redirect("/repositories");
+
+      // req.flash("error", err.message);
+      // res.redirect("/users/signup");
   }),
 );
 
